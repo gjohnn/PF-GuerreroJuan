6c47000d84500d5a90bad9b6c251652f31f53207
gjohn <juansgb316@gmail.com>
2022-12-20 20:18:45 -0300

    Trying 2nd PreEntrega

--- js/app.js
+++ js/app.js
@@ -1,52 +1,88 @@
-let nomprod = prompt("Ingrese el nombre del producto")
-while (nomprod==""){
-    nomprod = prompt("Ingrese el nombre correcto del producto");
-}
-let precio = parseInt(prompt("Ingrese el precio del producto"));
-let preciof= 0;
-let nada = NaN
-while ((precio<=0)){
-     precio = parseInt(prompt("Ingrese el precio correcto del producto"));
-}
-let cuota = parseInt(prompt("Ingrese las cuotas (3, 6 o 12) en las que desea pagar, ingrese 0 si desea en efectivo/transferencia bancaria."));
-while((cuota<0) ||(cuota>12)|| (cuota==1) || (cuota==2) || (cuota>=4 && cuota<=5) || (cuota>=7 && cuota<=11)){
-    cuota = parseInt(prompt("Ingrese las cuotas (3, 6 o 12) en las que desea pagar, ingrese 0 si desea en efectivo/transferencia bancaria."));
+
+let usu = prompt("Ingrese tipo de usuario (Cliente/Vendedor)")
+while (usu == ""){
+    usu = prompt("Ingrese tipo de usuario (Cliente/Vendedor)");
 }
 
-function efectivo(){
-    preciof = precio*0.90 
-}
+if (usu == ("cliente")|| usu==("Cliente")){
+    let nomprod = prompt("Ingrese el nombre del producto")
+    while (nomprod==""){
+        nomprod = prompt("Ingrese el nombre correcto del producto");
+    }
+    let precio = parseInt(prompt("Ingrese el precio del producto"));
+    
+    while ((precio<=0)){
+         precio = parseInt(prompt("Ingrese el precio correcto del producto"));
+    }
+    let cuota = parseInt(prompt("Ingrese las cuotas (3, 6 o 12) en las que desea pagar, ingrese 0 si desea en efectivo/transferencia bancaria."));
+    while((cuota<0) ||(cuota>12)|| (cuota==1) || (cuota==2) || (cuota>=4 && cuota<=5) || (cuota>=7 && cuota<=11)){
+        cuota = parseInt(prompt("Ingrese las cuotas (3, 6 o 12) en las que desea pagar, ingrese 0 si desea en efectivo/transferencia bancaria."));
+    }
+    
+    function cuotas(){
+        if (cuota==0){
+        precio = precio*0.90 
+        }else if (cuota==3){
+            precio = precio*1.15
+        }else if (cuota==6){
+            precio = precio*1.35 
+        }else if(cuota==12){
+            precio = precio*1.70
+        }
+        console.log("Precio final: "+precio);
+    }
+    
+    function showprod(){
+        console.log("Producto: "+nomprod);
+        console.log("Precio: $"+ precio);
+    }
+    showprod();
+    cuotas();
+}else if (usu == ("vendedor")||usu==("Vendedor")){
+  
+    let trabajadores = [
+        {id:37, emp: "Juan"},
+        {id:56, emp: "Martín"},
+        {id:12, emp: "María"},
+        {id:67, emp: "Ana"},
+        {id:41, emp: "Pablo"},
+    ]
 
-function cuota3(){
-    preciof = precio*1.15
-}
+    let empid = parseInt(prompt("Ingrese ID"))
+    
+    
 
-function cuota6(){
-    preciof = precio*1.35
-}
 
-function cuota12(){
-    preciof = precio*1.70
-}
+    //pusher
+    function cargarel(arr,el){
+        arr.push(el);
+    }
+    //MOTHERBOARD
+    const motherss = []
+    const mother1 = {nom:"H610", precio: 15000, stock:5};
+    const mother2 = {nom:"Z690", precio: 90000, stock:5};
+    const mother3 = {nom:"H610", precio: 15000, stock:5};
 
-function showprod(){
-    console.log("Producto: "+nomprod)
-    console.log("Precio: $"+ precio)
-}
+    cargarel(motherss,mother1);
+    cargarel(motherss,mother2);
+    cargarel(motherss,mother3);
 
-if (cuota == 0){
-        efectivo();
-        showprod();
-        console.log("10% de descuento: $"+preciof);
-}else if (cuota == 3){    
-        cuota3();
-        console.log( "15% de interés: $"+preciof);
-}else if (cuota == 6){
-        cuota6();
-        showprod();
-        console.log("35% de interés: $"+preciof);
-}else if (cuota == 12){
-        cuota12();
-        showprod();
-        console.log("70% de interés: $"+preciof);
+    for(const mother of motherss){
+        console.log(mother.nom);
+        console.log("$"+mother.precio);
+        console.log("Stock: "+mother.stock);
+    }
+    //GPU
+    const gpuss = [];
+    const gpu1 = {nom:"RTX 3070", precio:170000, stock:15};
+    const gpu2 = {nom:"RTX 3080 TI", precio:250000, stock:15};
+    const gpu3 = {nom:"RTX 4080", precio:400000, stock:15};
+    cargarel(gpuss,gpu1);
+    cargarel(gpuss,gpu2);
+    cargarel(gpuss,gpu3);
+    for(const gpu of gpuss){
+        console.log(gpu.nom);
+        console.log("$"+gpu.precio);
+        console.log("Stock: "+gpu.stock);
+    }
 }
